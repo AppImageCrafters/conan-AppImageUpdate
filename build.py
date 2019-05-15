12e305266a92a234398fb5820a3d41a93ae6ffbf
Alexis Lopez Zubieta <contact@azubieta.net>
2019-05-15 16:37:08 -0500

    Try building from sources

--- build.py
+++ build.py
@@ -1,12 +1,14 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
+    command = "sudo apt-get -qq update && sudo apt-get -qq install -y linux-libc-dev libxext-dev desktop-file-utils"
     remotes = [("https://api.bintray.com/conan/appimage-conan-community/public-conan", "true", "appimage"),
                ("https://api.bintray.com/conan/bincrafters/public-conan", "true", "bincrafters")]
-    builder = ConanMultiPackager(remotes=remotes, build_policy="missing", build_types=["Release"])
+    builder = ConanMultiPackager(remotes=remotes, build_policy="missing", build_types=["Release"], archs=["x86_64"],
+                                 docker_entry_script=command)
     builder.add_common_builds()
     for settings, options, env_vars, build_requires, reference in builder.items:
         settings["compiler.libcxx"] = "libstdc++11"
-        settings["cppstd"] = "11"
+        settings["compiler.cppstd"] = "11"
 
     builder.run()
--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, tools, errors
+from conans import ConanFile, tools, errors, CMake
 import os
 
 
@@ -11,8 +11,13 @@ class AppimageupdateConan(ConanFile):
     description = "AppImageUpdate lets you update AppImages in a decentral way using information embedded in the AppImage itself.   "
     topics = ("AppImage", "Update")
     settings = "os", "compiler", "build_type", "arch"
-    requires = ("zlib/1.2.11@conan/stable")
+    requires = ("OpenSSL/1.1.1b@conan/stable"), ("zlib/1.2.11@conan/stable"), ("libcurl/7.64.1@bincrafters/stable"), \
+               ("glib/2.40.0@appimage-conan-community/stable"), ("cairo/1.17.2@bincrafters/stable"), \
+               ("qt/5.12.3@appimage-conan-community/stable"), ("libpng/1.6.36@bincrafters/stable")
+    build_requires = ("cmake_installer/3.14.3@conan/stable")
     default_options = {"zlib:shared": True}
+    generators = "cmake"
+    exports_sources = "patches/*"
 
     def conanArchToSystem(self, conan_arch):
         if conan_arch == "x86":
@@ -30,37 +35,30 @@ class AppimageupdateConan(ConanFile):
         pkgs_name = None
         system_arch = self.conanArchToSystem(self.settings.arch)
         if tools.os_info.linux_distro == "ubuntu":
-            pkgs_name = ["linux-libc-dev:%s" % system_arch]
+            pkgs_name = ["linux-libc-dev:%s" % system_arch, "libxext-dev:%s" % system_arch]
 
         if pkgs_name:
             installer = tools.SystemPackageTool()
             for pkg_name in pkgs_name:
                 installer.install(pkg_name)
 
+    def source(self):
+        self.run("git clone https://github.com/AppImage/AppImageUpdate.git --branch=%s" % self.version)
+        self.run("cd AppImageUpdate && git submodule update --init --recursive")
+        tools.patch(base_path="AppImageUpdate", patch_file="patches/use_conan.patch")
+
     def build(self):
-        if (self.settings.build_type != "Release"):
-            raise errors.ConanInvalidConfiguration("Only Release builds are supported.")
-
-        if (self.settings.arch == "x86" or self.settings.arch == "x86_64"):
-            # match the arch naming convention used in AppImageKit
-            appimagetool_arch_name = "i386" if self.settings.arch == "x86" else self.settings.arch
-
-            tools.download(
-                "https://github.com/AppImage/AppImageUpdate/releases/download/%s/appimageupdatetool-%s.AppImage"
-                % (self.version, appimagetool_arch_name), "appimageupdatetool-%s.AppImage" % appimagetool_arch_name)
-        else:
-            raise errors.ConanInvalidConfiguration("Unsuported arch: %s" % self.settings.arch)
-
-        self.run("chmod +x appimageupdatetool-%s.AppImage && ./appimageupdatetool-%s.AppImage --appimage-extract"
-                 % (appimagetool_arch_name, appimagetool_arch_name), run_environment=True)
-
-        tools.download("https://raw.githubusercontent.com/AppImage/AppImageUpdate/continuous/include/appimage/update.h",
-                       "update.h")
+        cmake = CMake(self)
+        cmake.definitions["USE_SYSTEM_GTEST"] = "OFF"
+        cmake.definitions["CMAKE_USE_OPENSSL"] = "OFF"
+        cmake.configure(source_folder="AppImageUpdate")
+        cmake.build()
 
     def package(self):
-        self.copy("*", dst="bin", src="squashfs-root/usr/bin")
-        self.copy("*", dst="lib", src="squashfs-root/usr/lib")
-        self.copy("update.h", dst="include/appimage")
+        self.copy("libappimageupdate*.so", src="lib", dst="lib", keep_path=False)
+        self.copy("libappimageupdate*.a", src="lib", dst="lib", keep_path=False)
+        self.copy("*", src="bin", dst="bin", keep_path=False)
+        self.copy("AppImageUpdate/include/appimage/update.h", dst="include/appimage", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
